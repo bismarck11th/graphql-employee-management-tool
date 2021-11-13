11300cf58980347d70619fdad0d72b84039372e6
kazuki827 <xm.kazuki827@gmail.com>
2021-11-13 15:38:54 +0900

    create apollo client

---
+++ front/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { Auth, MainPage } from './components';
+import styles from './App.module.css';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8000/graphql',
+  headers: {
+    authorization: localStorage.getItem('token')
+      ? `JWT ${localStorage.getItem('token')}`
+      : ''
+  },
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className={styles.app}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Auth} />
+            <Route exact path="/employees" component={MainPage} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
